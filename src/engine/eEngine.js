c69004a96f637b7dba2334b2e58c1dd50c6429c1
Allan Bonadio <bojnac@tactileint.org>
2022-10-04 11:47:57 -0700

    renamed start promise to eSpaceCreatedPromise now handles errors in the promise tracing

--- src/engine/eEngine.js
+++ src/engine/eEngine.js
@@ -4,6 +4,8 @@
 */
 
 import {qe, defineQEngineFuncs} from './qe';
+import eSpace from './eSpace';
+import {getASetting} from '../utils/storeSettings';
 
 // all of these must be attached to window to  get called by c++
 
@@ -15,7 +17,33 @@ let tracePromises = false;
 // (qspace can change; double and therefore eCx can change length)
 export let maxDimensions, maxLabel;
 
-let qeStartPromiseSucceed;
+// this resolves when emscripten starts up.  Create it tout suit when app starts.
+// Also, importing this guarantees this file is included in the build!
+// it's exported so everybody can get to it.
+// set again if space recreated, then resolved again.  Therefore, don't pass this
+// around; don't keep iit on this, always get it from import!
+// Initially set to a promise that'll never get resolved until crate1DMainSpace() called.
+let eSpaceCreatedSucceed;
+let eSpaceCreatedFail;
+export let eSpaceCreatedPromise = new Promise((succeed, fail) => {
+	eSpaceCreatedSucceed = succeed;
+	eSpaceCreatedFail = fail;
+	if (tracePromises) console.info(`eSpaceCreatedPromise (re)created:`, succeed, fail);
+});
+
+// params is {N, continuum, label: 'x'}  label=label for this dimension, not the whole thing
+export function create1DMainSpace(params) {
+	try {
+		// eSpace expects an array of param sets, one for each dimension
+		let space = new eSpace([params], 'main');
+
+		// wakes up stuff all over the JS, and gives them the space,
+		// that they've been waiting for
+		eSpaceCreatedSucceed(space);
+	} catch (ex) {
+		eSpaceCreatedFail(ex);
+	}
+}
 
 // do NOT export this; it's global cuz quantumEngine.js, the compiled C++ proxy,
 // has to have access to it early on
@@ -24,22 +52,13 @@ function quantumEngineHasStarted(mDimensions, mLabel) {
 	defineQEngineFuncs();
 	//qeDefineAccess();
 
-	maxDimensions = mDimensions;
-	maxLabel = mLabel;
-
 	qe.cppLoaded = true;
 
-	if (tracePromises) console.log(`quantumEngineHasStarted:  resolving qeStartPromise`);
-	qeStartPromiseSucceed({mDimensions, mLabel});
-};
+	create1DMainSpace({N: getASetting('spaceParams', 'N'), continuum: getASetting('spaceParams', 'continuum'), label: 'main'});
 
-// this is how enscripten/C++ gets to it
+	if (tracePromises) console.log(
+		`quantumEngineHasStarted:  space created and resolving eSpaceCreatedPromise`);
+};
 window.quantumEngineHasStarted = quantumEngineHasStarted;
 
 
-// this resolves when emscripten starts up.
-// Also, importing this (in SquishPanel) guarantees this file is included in the build!
-export const qeStartPromise = new Promise((succeed, fail) => {
-	qeStartPromiseSucceed = succeed;
-	if (tracePromises) console.info(`qeStartPromise created:`, succeed, fail);
-});
