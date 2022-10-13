3b7311f3f075218b3528c0316dbf724ead541d37
Allan Bonadio <bojnac@tactileint.org>
2022-10-12 17:49:07 -0700

    got it to WORK!

--- quantumEngine/schrodinger/qAvatar.spec.cpp
+++ quantumEngine/schrodinger/qAvatar.spec.cpp
@@ -21,7 +21,7 @@ TEST_GROUP(qAvatar)
 
 /* ******************************************************************************************** boring constructor tests */
 // just the constructor; it's not even fully created
-TEST(qAvatar, CheckConstructor)
+TEST(qAvatar, qAvatar_CheckConstructor)
 {
 	qSpace *space = makeBareSpace(8, contENDLESS);
 	qAvatar *avatar = new qAvatar(space, "CheckConst");
@@ -40,37 +40,42 @@ TEST(qAvatar, CheckConstructor)
 // boring, just copied from the space test script
 
 // this tests the whole shebang, as created from JS
-static void completeNewAvatarGauntlet(int N, int expectedSpectrumLength) {
-	qSpace *space = makeFullSpace(N);
+static void completeNewAvatarGauntlet(int N) {
+	qSpace *space = makeBareSpace(N);
+	qAvatar *avatar = new qAvatar(space, "AvGa");
 	int nPoints = space->nPoints;
 
-	LONGS_EQUAL(0, theAvatar->elapsedTime);
-	LONGS_EQUAL(0, theAvatar->iterateSerial);
-	//pointless DOUBLES_EQUAL(1.0, theAvatar->dt, 1e-12);
-	LONGS_EQUAL(100, theAvatar->stepsPerIteration);
-	DOUBLES_EQUAL(N/8, theAvatar->lowPassFilter, 1e-12);
-	LONGS_EQUAL(false, theAvatar->isIterating);
+	LONGS_EQUAL(0, avatar->elapsedTime);
+	LONGS_EQUAL(0, avatar->iterateSerial);
+	//pointless DOUBLES_EQUAL(1.0, avatar->dt, 1e-12);
+	LONGS_EQUAL(100, avatar->stepsPerIteration);
+	// long story DOUBLES_EQUAL(N/8, avatar->lowPassFilter, 1e-12);
+	LONGS_EQUAL(false, avatar->isIterating);
 
 
 	// lets see if the buffers are all large enough
-	proveItsMine(theAvatar->mainQWave->wave	, nPoints * sizeof(qCx));
+	proveItsMine(avatar->mainQWave->wave	, nPoints * sizeof(qCx));
+	proveItsMine(avatar->qvBuffer->vBuffer, nPoints * sizeof(float) * 8);
 
-	theAvatar->getScratchWave();
-	proveItsMine(theAvatar->scratchQWave->wave, nPoints * sizeof(qCx));
+	qWave *scrWave = avatar->getScratchWave();
+	CHECK_TEXT(scrWave == avatar->scratchQWave, "scratch wave not the same");
+	proveItsMine(avatar->scratchQWave->wave, nPoints * sizeof(qCx));
 
-	theAvatar->getSpectrum();
-	proveItsMine(theAvatar->qvBuffer->vBuffer, nPoints * sizeof(float) * 8);
+	qSpectrum *qspect = avatar->getSpectrum();
+	CHECK_TEXT(qspect == avatar->qspect, "spectrum not the same");
+	qCx *swave = qspect->wave;
+	proveItsMine(swave, space->nStates * sizeof(qCx));
 
-	// will also delete the avatar and other buffers
-	deleteTheSpace();
+	delete avatar;
+	delete space;
 }
 
-TEST(qAvatar, Avatar_completeNewAvatarGauntlet4000) { completeNewAvatarGauntlet(4096, 4096); }
-TEST(qAvatar, Avatar_completeNewAvatarGauntlet254) { completeNewAvatarGauntlet(256, 256); }
-TEST(qAvatar, Avatar_completeNewAvatarGauntlet63) { completeNewAvatarGauntlet(64, 64); }
-TEST(qAvatar, Avatar_completeNewAvatarGauntlet32) { completeNewAvatarGauntlet(32, 32); }
-TEST(qAvatar, Avatar_completeNewAvatarGauntlet32x) { completeNewAvatarGauntlet(32, 32); }
-TEST(qAvatar, Avatar_completeNewAvatarGauntlet4) { completeNewAvatarGauntlet(4, 4); }
+TEST(qAvatar, qAvatar_completeNewAvatarGauntlet4096) { completeNewAvatarGauntlet(4096); }
+TEST(qAvatar, qAvatar_completeNewAvatarGauntlet256) { completeNewAvatarGauntlet(256); }
+TEST(qAvatar, qAvatar_completeNewAvatarGauntlet64) { completeNewAvatarGauntlet(64); }
+TEST(qAvatar, qAvatar_completeNewAvatarGauntlet32) { completeNewAvatarGauntlet(32); }
+TEST(qAvatar, qAvatar_completeNewAvatarGauntlet32x) { completeNewAvatarGauntlet(32); }
+TEST(qAvatar, qAvatar_completeNewAvatarGauntlet4) { completeNewAvatarGauntlet(4); }
 
 
 
@@ -80,7 +85,7 @@ static bool traceFourierFilter = true;
 
 // try out FF on a mix of frequencies; should filter out
 // goodFreq should stay there, while badFreq should be filtered out.
-static void tryFourierFilter(int N, int goodFreq, int badFreq, int lowPassFilter)
+static void tryFourierFilter(int N, int goodFreq, int badFreq, int lowPassFilter, bool shouldFail)
 {
 	if (traceFourierFilter) printf("🌈  tryFourierFilter(N=%d, freq=%d & %d,  lowPassFilter=%d)\n",
 		 N,  goodFreq, badFreq,  lowPassFilter);
@@ -105,9 +110,7 @@ static void tryFourierFilter(int N, int goodFreq, int badFreq, int lowPassFilter
 	rainbow->dumpSpectrum("tryFourierFilter() done");
 	//rainbow->dumpHiRes("tryFourierFilter() done");
 
-	isAllZeroesExceptFor(rainbow, goodFreq);
-
-
+	CHECK(shouldFail ^ isAllZeroesExceptFor(rainbow, goodFreq));
 	//qw->dump("avatar wave AFTER fourierFilter()", true);
 
 	delete avatar;
@@ -120,24 +123,31 @@ static void tryFourierFilter(int N, int goodFreq, int badFreq, int lowPassFilter
 // example: 16 states.  Initially has waves of frequencies 3 & 6.
 // filter out frequencies 5, 6, 7, ... *8* ... 9, 10, 11.   8 is the nyquist freq
 // So expect only freq 3 left, value=4 on fft.
-//TEST(qAvatar, fourierFilter16_3_6) { tryFourierFilter(16, 3, 6, 3); }
+//TEST(qAvatar, qAvatar_foFi16_3_6) { tryFourierFilter(16, 3, 6, 3); }
 //
-//TEST(qAvatar, fourierFilter16_1_8) { tryFourierFilter(16, 1, 8, 1); }
+//TEST(qAvatar, qAvatar_foFi16_1_8) { tryFourierFilter(16, 1, 8, 1); }
 //
-//TEST(qAvatar, fourierFilter16_2_7) { tryFourierFilter(16, 2, 7, 3); }
+//TEST(qAvatar, qAvatar_foFi16_2_7) { tryFourierFilter(16, 2, 7, 3); }
 
 
 // ok we have a freq 1 carrier.  We stick in a freq 5 noise.  When does it cut off?
-TEST(qAvatar, fourierFilter16_1_LPFscan2) { tryFourierFilter(16, 1, 5, 2); }
-TEST(qAvatar, fourierFilter16_1_LPFscan3) { tryFourierFilter(16, 1, 5, 3); }
-TEST(qAvatar, fourierFilter16_1_LPFscan4) { tryFourierFilter(16, 1, 5, 4); }
-TEST(qAvatar, fourierFilter16_1_LPFscan5) { tryFourierFilter(16, 1, 5, 5); }
-TEST(qAvatar, fourierFilter16_1_LPFscan6) { tryFourierFilter(16, 1, 5, 6); }
-TEST(qAvatar, fourierFilter16_1_LPFscan7) { tryFourierFilter(16, 1, 5, 7); }
+// these are not cut off
+TEST(qAvatar, qAvatar_foFi16_1_LPFscan0) { tryFourierFilter(16, 1, 5, 0, true); }
+TEST(qAvatar, qAvatar_foFi16_1_LPFscan1) { tryFourierFilter(16, 1, 5, 1, true); }
+TEST(qAvatar, qAvatar_foFi16_1_LPFscan2) { tryFourierFilter(16, 1, 5, 2, true); }
 
-//TEST(qAvatar, fourierFilter16_1_3_LPFscan7) { tryFourierFilter(16, 1, 3, 7); }
+// these ARE cut off
+TEST(qAvatar, qAvatar_foFi16_1_LPFscan3) { tryFourierFilter(16, 1, 5, 3, false); }
+TEST(qAvatar, qAvatar_foFi16_1_LPFscan4) { tryFourierFilter(16, 1, 5, 4, false); }
+TEST(qAvatar, qAvatar_foFi16_1_LPFscan5) { tryFourierFilter(16, 1, 5, 5, false); }
+TEST(qAvatar, qAvatar_foFi16_1_LPFscan6) { tryFourierFilter(16, 1, 5, 6, false); }
 
-//TEST(qAvatar, fourierFilter64_5_22) { tryFourierFilter(64, 5, 22, 16); }
+// also wipes out freq 1 so should fail the test
+TEST(qAvatar, qAvatar_foFi16_1_LPFscan7) { tryFourierFilter(16, 1, 5, 7, true); }
+
+//TEST(qAvatar, qAvatar_foFi16_1_3_LPFscan7) { tryFourierFilter(16, 1, 3, 7); }
+
+//TEST(qAvatar, qAvatar_foFi64_5_22) { tryFourierFilter(64, 5, 22, 16); }
 
 
 /* ********************************************************************** random experimentatiojn */
@@ -168,7 +178,7 @@ static void fourierExperiments(int N) {
 	delete space;
 }
 
-TEST(qAvatar, fourierExperiments)
+IGNORE_TEST(qAvatar, fourierExperiments)
 {
 	fourierExperiments(16);
 
