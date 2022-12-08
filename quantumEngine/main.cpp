04d356a53f7bc997b5975ffab26799ad6f7f90c5
Allan Bonadio <bojnac@tactileint.org>
2022-12-07 18:36:56 -0800

    qeStarted now returns an int, hardwareConcurrency = n cpus now catches when hot reload screws things up and does a page reload

--- quantumEngine/main.cpp
+++ quantumEngine/main.cpp
@@ -18,10 +18,16 @@
 // call this JS callback so JS knows we're up and ready.
 // Hand it some numbers from the builder script.
 // somehow there's a race condition where this isn't set soon enough... sometimes
-EM_JS(void, qeStarted, (int max_dimensions, int max_label_len),
+EM_JS(int, qeStarted, (int max_dimensions, int max_label_len),
 {
 	// maybe we can tighten this up a bit someday
-	setTimeout(() => quantumEngineHasStarted(max_dimensions, max_label_len), 50);
+	setTimeout(() =>{
+		if (window.quantumEngineHasStarted)
+			window.quantumEngineHasStarted(max_dimensions, max_label_len);
+		else
+			location = location;  // restart the page cuz hotreloading sabotaged me again
+	}, 50);
+	return navigator.hardwareConcurrency;
 }
 );
 
@@ -32,8 +38,8 @@ int main() {
 
 	// returns 1.  pfft.  std::thread::hardware_concurrency();
 
-	qeStarted(MAX_DIMENSIONS, MAX_LABEL_LEN);
-
+	int hardwareConcurrency = qeStarted(MAX_DIMENSIONS, MAX_LABEL_LEN);
+	printf("hardwareConcurrency=%d  \n", hardwareConcurrency);
 	//printf("alignof(double) = %ld, alignof(int) = %ld, alignof(int *) = %ld, alignof(bool) = %ld\n",
 	//	alignof(double), alignof(int), alignof(int *), alignof(bool));
 	return 0;
