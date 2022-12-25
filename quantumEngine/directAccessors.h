c3f40fa2fda45f738cfd6a902b4bc43170afb4dc
Allan Bonadio <bojnac@tactileint.org>
2022-12-24 16:50:56 -0800

    now prefixes all accessors with tab.

--- quantumEngine/directAccessors.h
+++ quantumEngine/directAccessors.h
@@ -4,32 +4,34 @@
 */
 
 // see how these are used in qAvatar.cpp and qWave.cpp
-// use one of these printf macros for each field from your .h file you want to export.
+// use one of these printf macros, in obj::formatDirectOffsets(),  for each field from your .h file you want to export to JS.
 // You should arrange the fields in the .h file from
 // wides (doubles) to narrows (bools) for safer alignment.
-// Include or omit fields and setters as appropriate when you use these macros in a
+// Include or omit fields and setters as appropriate when you use these macros in an object
+// Best to run this in the shell with unit tests, but the browser is OK.
+// then, copy output and paste it into the corresponding  .js source.
 
 // use for bool field, or anything 1 byte
 #define byteOffset(field)  (int) ((byte *) &this->field - (byte *) this)
-#define makeBoolGetter(field)  printf("get " #field  "() { return this.bools[%d]; }\n", byteOffset(field));
-#define makeBoolSetter(field)  printf("set " #field  "(a) { this.bools[%d] = a; }\n", byteOffset(field));
+#define makeBoolGetter(field)  printf("\tget " #field  "() { return this.bools[%d]; }\n", byteOffset(field));
+#define makeBoolSetter(field)  printf("\tset " #field  "(a) { this.bools[%d] = a; }\n", byteOffset(field));
 
 // use for a standard C string
-#define makeStringPointer(field)  printf("get _" #field  "() { return this.pointer + %d; }\n", byteOffset(field));
+#define makeStringPointer(field)  printf("\tget _" #field  "() { return this.pointer + %d; }\n", byteOffset(field));
 
 // use for int field, or anything 32 bits, like a pointer
 #define intOffset(field)  (int) ((int *) &this->field - (int *) this)
-#define makeIntGetter(field)  printf("get " #field  "() { return this.ints[%d]; }\n", intOffset(field));
-#define makeIntSetter(field)  printf("set " #field  "(a) { this.ints[%d] = a; }\n", intOffset(field));
+#define makeIntGetter(field)  printf("\tget " #field  "() { return this.ints[%d]; }\n", intOffset(field));
+#define makeIntSetter(field)  printf("\tset " #field  "(a) { this.ints[%d] = a; }\n", intOffset(field));
 
 // like makeIntGetter() but creates a different name so as to not conflict with actual JS field in same class.
 // This will return to JS the C++ pointer value, which can be wrapped into a JS class.
 // No corresponding setter cuz only C++ sets the pointers in constructors or other
-#define makePointerGetter(field)  printf("get _" #field  "() { return this.ints[%d]; }\n", intOffset(field));
+#define makePointerGetter(field)  printf("\tget _" #field  "() { return this.ints[%d]; }\n", intOffset(field));
 
 // use for double float field, or anything 64 bits
 #define doubleOffset(field)  (int) ((double *) &this->field - (double *) this)
-#define makeDoubleGetter(field)  printf("get " #field  "() { return this.doubles[%d]; }\n", doubleOffset(field));
-#define makeDoubleSetter(field)  printf("set " #field  "(a) { this.doubles[%d] = a; }\n", doubleOffset(field));
+#define makeDoubleGetter(field)  printf("\tget " #field  "() { return this.doubles[%d]; }\n", doubleOffset(field));
+#define makeDoubleSetter(field)  printf("\tset " #field  "(a) { this.doubles[%d] = a; }\n", doubleOffset(field));
 
 
