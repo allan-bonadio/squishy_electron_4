f0bd3af12d0b17cd242b8ee34e378837080853cb
Allan Bonadio <bojnac@tactileint.org>
2022-12-25 21:42:59 -0800

    removed unnecessary and obsolete junk

--- quantumEngine/debroglie/qWave.h
+++ quantumEngine/debroglie/qWave.h
@@ -17,10 +17,6 @@ struct qWave : public virtual qBuffer {
 
 	virtual ~qWave();
 
-	// used for low pass; need general buffer for arithmetic.  obsolete.
-	// and qWave::nyquistFilter().  See scratchQWave in avatar
-	qCx *scratchBuffer;
-
 	// never even tried any of these
 	void forEachPoint(void (*callback)(qCx, int));
 	void forEachState(void (*callback)(qCx, int));
@@ -38,7 +34,7 @@ struct qWave : public virtual qBuffer {
 // for JS to call
 extern "C" {
 	// js thinks it's a qWave but we know it's really a qBuf cuz that's where
-	// the method is.  Maybe I shouldn't be doing this?
+	// the method is.  JS isn't using this, some problem..?
 	void wave_normalize(qWave *qw);
 }
 
