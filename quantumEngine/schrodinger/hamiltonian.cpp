0c1adbc896ff9804e51900a060706115616e4da2
Allan Bonadio <bojnac@tactileint.org>
2023-01-29 23:44:08 -0800

    whole thing is so way out of date i commented everything out

--- quantumEngine/schrodinger/hamiltonian.cpp
+++ quantumEngine/schrodinger/hamiltonian.cpp
@@ -14,20 +14,21 @@
 // this is only for continuum dimension.  Ultimately, these should be per-dimension,
 // and each dimension should have a function that does the honors.
 // or, per-view or per-space.
-// btw, this is really Hψ not just H.  Isn't H supposed to be real?
+// btw, this is really Hψ not just H.  Isn't H supposed to be real?  real eigenvalues
 // real eigenvalues.
-qCx hamiltonian(qCx *wave, int ix) {
-	// so at location ix, if dx = 1,
-	// the derivative would be (𝜓[ix+1] - 𝜓[ix])
-	//                      or (𝜓[ix] - 𝜓[ix-1])
-	// so second deriv would be 𝜓[ix+1] + 𝜓[ix-1] - 2* 𝜓[ix]
-	qCx d2 = wave[ix-1] + wave[ix+1] - wave[ix] * 2;
-	qCheck(d2, "hamiltonian d2", ix);
-
-	qCx pot = wave[ix] * theVoltage[ix];
-	qCheck(pot, "hamiltonian pot", ix);
-	qCx rate = pot - d2;
-
-	return rate;
-}
+//					qCx hamiltonian(qSpace *space, qCx *wave, int ix) {
+//						// so at location ix, if dx = 1,
+//						// the derivative would be (𝜓[ix+1] - 𝜓[ix])
+//						//                      or (𝜓[ix] - 𝜓[ix-1])
+//						// so second deriv would be 𝜓[ix+1] + 𝜓[ix-1] - 2* 𝜓[ix]
+//						qCx d2 = wave[ix-1] + wave[ix+1] - wave[ix] * 2;
+//						qCheck(d2, "hamiltonian d2", ix);
+//
+//						// theVoltage no longer e3xists qCx pot = wave[ix] * theVoltage[ix];
+//						double pot = space->voltageBuffer[ix];
+//						qCheck(pot, "hamiltonian pot", ix);
+//						qCx rate = pot - d2;
+//
+//						return rate;
+//					}
 
