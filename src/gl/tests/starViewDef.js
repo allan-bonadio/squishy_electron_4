3c16607ce10ce28c3bcbb97e7bb3de44c6becc64
Allan Bonadio <bojnac@tactileint.org>
2023-02-13 00:28:28 -0800

    It works now! And switches colors as it draws. cool. But now we can use it and forks of it for testing. took out large blocks of commented-out code

--- src/gl/tests/starViewDef.js
+++ src/gl/tests/starViewDef.js
@@ -3,185 +3,144 @@
 ** Copyright (C) 2021-2023 Tactile Interactive, all rights reserved
 */
 
-import {abstractViewDef} from './abstractViewDef.js';
-import {abstractDrawing} from './abstractDrawing.js';
-import {viewUniform, viewAttribute} from './viewVariable.js';
+import {abstractViewDef} from '../abstractViewDef.js';
+import {abstractDrawing} from '../abstractDrawing.js';
+import {viewUniform, viewAttribute} from '../viewVariable.js';
 
 
-// NOT DEBUGGED YET!!!
+let corners;
 
+// create this table the way viewAttribute likes it
+function createCorners() {
+	// create the data for the corners attribute
+	const sin = Math.sin;
+	const cos = Math.cos;
+	corners = new Float32Array([
+		cos(1), sin(1),
+		cos(3), sin(3),
+		cos(5), sin(5),
+		cos(7), sin(7),
+		cos(9), sin(9),
+		cos(11), sin(11),
+		cos(0), sin(0),
+		cos(2), sin(2),
+		cos(4), sin(4),
+		cos(6), sin(6),
+		cos(8), sin(8),
+		cos(10), sin(10),
+	]);
+	corners.nTuples = corners.length / 2;
+}
 
-// this makes a light green color if false, a little on the yellow side.
-// if true, you should see something brighter and less yellow
-let includeUniform = true;
+// keep these #lines pointing to the next line number in the file!
+const vertexSrc = `
+#line 43
+precision highp float;
+uniform vec4 cornerColorUni;
+varying vec4 nuColor;
 
+attribute vec2 corner;
+//attribute vec4 corner;
+
+// always returns 0...1
+float scramble(float a) {
+	return fract(exp(a + 7.12));
+}
+
+void main() {
+	gl_Position = corner.xyyy;
+	gl_Position.zw = vec2(0., 1.);
+
+	nuColor.r = scramble(corner.x);
+	nuColor.g = scramble(corner.y + cornerColorUni.g);
+	nuColor.b = scramble(corner.x + corner.y);
+	nuColor.a = scramble(corner.x - corner.y);
+
+	gl_PointSize = 10.;  // dot size, actually a crude square
+}
+`;
+
+const fragmentSrc = `
+#line 70
+precision highp float;
+uniform vec4 cornerColorUni;
+varying vec4 nuColor;
+
+void main() {
+	// colored triangle, depends on the uniform?
+	gl_FragColor = nuColor;
+	//gl_FragColor = cornerColorUni;
+	//gl_FragColor = vec4(0., .5, 1., 1.);
+}
+`;
 
 export class starDrawing extends abstractDrawing {
 
 	constructor(viewDef) {
 		super(viewDef, 'starDrawing');
 
-		this.vertexShaderSrc = `
-		attribute vec4 corner;
-		void main() {
-			gl_Position = corner;
-
-			gl_PointSize = 10.;  // dot size, actually a crude square
-		}
-		`;
-
-		const decl = includeUniform ? `uniform vec4 cornerColorUni;` : '';
-		const cornerColorUni = includeUniform ? `cornerColorUni` : 'vec4(0.,.5,1.,1.)';
-		this.fragmentShaderSrc = `
-		precision highp float;
-		${decl}
-
-		void main() {
-			// colored triangle, depends on the uniform?
-			gl_FragColor = ${cornerColorUni};
-		}
-		`;
+		this.vertexShaderSrc = vertexSrc;
+		this.fragmentShaderSrc = fragmentSrc;
 	}
 
 	// all to do this one differently
 	createVariables() {
-		//const gl = this.gl;
+		this.setDrawing();
+		//debugger;
 
-		//let cornerColorUni;
-		if (includeUniform) {
-			//cornerColorUni =
-			this.cornerColorUni =
-				new viewUniform('cornerColorUni', this,
-					() => ({value: [0, 1, .5, 1],   // teal
-						type: '4fv'}) );
-		}
+		this.cornerColorUni =
+			new viewUniform('cornerColorUni', this,
+				() => {
+					return {value: [0, 1, .5, 1], type: '4fv'}
+				}
+			);
 
 //		const cornerAttributeLocation = gl.getAttribLocation(this.program, 'corner');
 //		const cornerBuffer = gl.createBuffer();  // actual ram in GPU chip
 //		gl.bindBuffer(gl.ARRAY_BUFFER, cornerBuffer);
 
-		// create the data for the corners attribute
-		const sin = Math.sin;
-		const cos = Math.cos;
-		const corners = new Float32Array([
-			cos(1), sin(1),
-			cos(3), sin(3),
-			cos(5), sin(5),
-			cos(7), sin(7),
-			cos(9), sin(9),
-			cos(11), sin(11),
-			cos(0), sin(0),
-			cos(2), sin(2),
-			cos(4), sin(4),
-			cos(6), sin(6),
-			cos(8), sin(8),
-			cos(10), sin(10),
-		]);
+
+		createCorners();
 
 		this.cornerAttr = new viewAttribute('corner', this, 2, () => corners);
 		//this.cornerAttr.attachArray(corners, 2);
 	}
 
-//	createVariables() {
-//		// loads view buffer from corresponding wave, calculates highest norm, which we use below.
-//		const highest = this.avatar.loadViewBuffer();
-//
-//		// smooth it out otherwise the wave sortof bounces up and down a little on each step
-//		// must find a way to set the avgHighest
-//		if (!this.avgHighest)
-//			this.avgHighest = highest;
-//		else
-//			this.avgHighest = (highest + 3*this.avgHighest) / 4;
-//		if (traceHighest)
-//			console.log(`starDrawing: highest=${highest.toFixed(5)}  avgHighest=${this.avgHighest.toFixed(5)}`);
-//
-//		let maxHeightUniform = this.maxHeightUniform = new viewUniform('maxHeight', this);
-//		maxHeightUniform.setValue(() => {
-//			return {value: this.avgHighest, type: '1f'};
-//		});
-//
-//		let barWidthUniform = this.barWidthUniform = new viewUniform('barWidth', this);
-//		let nPoints = this.nPoints = this.space.nPoints;
-//		let barWidth = 1 / (nPoints - 1);
-//		barWidthUniform.setValue(barWidth, '1f');
-//
-//		this.rowAttr = new viewAttribute('row', this);
-//		this.vertexCount = nPoints * 2;  // nPoints * vertsPerBar
-//		this.rowFloats = 4;
-//		this.rowAttr.attachArray(this.space.mainVBuffer, this.rowFloats);
-//	}
-
 	draw() {
 		const gl = this.gl;
+		this.setDrawing();
+		//debugger;
 
 		// is this a good place to do this?
 		gl.lineWidth(1.0);  // it's the only option anyway
 
-		gl.clearColor(0, 0, .3, 0);
-		gl.clear(gl.COLOR_BUFFER_BIT);
+		this.cornerColorUni.reloadVariable();
 
-		if (includeUniform) {
-			this.cornerColorUni.reloadVariable();
-		}
+		// for EACH type, that's enabled, draw with it.  fun!
+		starViewDef.typesList.forEach(type => {
 
-		// shouldn't need gl.useProgram(this.program);
-		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 12);
-
-
-		gl.drawArrays(gl.LINE_STRIP, 0, 12);
-		gl.drawArrays(gl.POINTS, 0, 12);
-
-		//gl.POINTS,     // most useful and foolproof but set width in vertex shader
-		//gl.LINES,      // tend to scribble all over
-		//gl.LINE_STRIP, // tend to scribble all over
-		//gl.TRIANGLES,  // more sparse triangles
+			gl.drawArrays(gl[type], 0, corners.nTuples);
+		})
 	}
 
-
-//	draw() {
-//		const gl = this.gl;
-//
-//		gl.useProgram(this.program);
-//		//this.rowAttr.reloadVariable()
-//
-//		//gl.bindVertexArray(this.vao);
-//		gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.vertexCount);
-//
-//		if (alsoDrawLines) {
-//			gl.lineWidth(1);  // it's the only option anyway
-//
-//			gl.drawArrays(gl.LINES, 0, this.vertexCount);
-//			//gl.drawArrays(gl.LINE_STRIP, 0, this.vertexCount);
-//		}
-//
-//		if (alsoDrawPoints)
-//			gl.drawArrays(gl.POINTS, 0, this.vertexCount);
-//
-//		// i think this is problematic
-//		if (dumpViewBufAfterDrawing)
-//			this.avatar.dumpViewBuffer(`finished drawing in starDrawing; drew buf:`);
-//	}
 }
+
+/* **************************************************************** Star View Def */
 
 
 export class starViewDef extends abstractViewDef {
-	static displayName: 'star';
+	static displayName = 'Star View';
 
 	constructor(viewName, glview, space, avatar) {
 		super(viewName, glview, space, avatar);
 
 		if (! this.space || !this.avatar) {
-			debugger;
 			throw  new Error(`flatDrawingViewDef: being created without space or avatar`);
 		}
 
 		// create relevant drawings
-		new starDrawing(this, space);
+		this.drawings = [new starDrawing(this)];
 	}
-
-
-
-
 }
 
 export default starViewDef;
