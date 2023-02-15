7553c9dece71d731dc99c689acb7180a796e68f1
Allan Bonadio <bojnac@tactileint.org>
2023-02-15 08:43:46 -0800

    add msgs with regexes for bbedit

--- maint/scanForTraces.sh
+++ maint/scanForTraces.sh
@@ -3,13 +3,16 @@
 echo "Scan sources for traces i left on... returns failure if there's some"
 echo "🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦  C++"
 cd $SQUISH_ROOT/quantumEngine
-grep   -ERn 'bool trace.* = true;' * && exit 41
+grep   -ERn 'bool trace.* = true;' * && \
+	echo "Try global regex search     bool trace.* = true;  "
 
 echo
 echo "🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦  JavaScript"
 
 cd $SQUISH_ROOT/src
-grep  -ERn '^(let|const) trace.* = true;' * && exit 43
+grep  -ERn '^(let|const) trace.* = true;' * && \
+	echo "Try global regex search     ^(let|const) trace.* = true;  "
+
 echo "🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 done scaning sources"
 
 
