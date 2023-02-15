302cdec93976f24068a7e32db81b8ed3ee85629e
Allan Bonadio <bojnac@tactileint.org>
2023-02-15 08:43:46 -0800

    made mechanism so I can turn on/off webgl-lint with one change to one line. Then, commented it all off. Added var to glAux to tell if i'm using webgl 1 or 2 added event handlers for the contextlost events; see if I get any msgs from that

--- src/gl/glAux.js
+++ src/gl/glAux.js
@@ -3,18 +3,22 @@
 ** Copyright (C) 2023-2023 Tactile Interactive, all rights reserved
 */
 
-// go thru this kicking and screaming cuz the tests aren't tuned in to node_modules
+// this should be renamed to 'glContext' or similar
+
+// webgl-lint: sigh.
+// the gl Tests aren't tuned in to node_modules; use the https form for those.
+// the app is fine with it, so use the regular form.
+// Or comment out both, to disable webgl-lint entirely.
 //import 'webgl-lint';
-import 'https://greggman.github.io/webgl-lint/webgl-lint.js';
+//import 'https://greggman.github.io/webgl-lint/webgl-lint.js';
 
 //import {tooOldTerminate} from '../utils/errors.js';
 
-let traceVersion = true;
+let traceVersion = false;
 
 /* ********************************************************** too old error */
 
-// call this if the browser/machine are just way too old to support the stuff we use:
-// what = 'WebGL' at least v1, 'WebAssembly', dedicated 'WebWorkers', ...
+// call this if the browser/machine are just way too old to support webgl
 export function tooOldTerminate(what) {
 	let tooOldMessage = `Sorry, your browser is too old for WebGL.
 		That's really really old!!
@@ -29,12 +33,14 @@ export function tooOldTerminate(what) {
 
 /* ********************************************************** actual glAux class */
 
+// create one of these for each canvas, to get the gl context, and other prep.
 class glAux {
-	// this decides it
+	// this decides it - feel free to change this
 	static preferWebGL2 = true;
 
 	constructor(canvas) {
 		this.canvas = canvas;
+		this.attachEventHandlers();  // might return errors about context creation/loss
 
 		if (glAux.preferWebGL2)
 			this.setupGL2(canvas);
@@ -45,19 +51,21 @@ class glAux {
 		if (!this.gl)
 			tooOldTerminate(`Sorry, your browser's WebGL is kinidof old.`);
 
-		// webgl-lint extension, needs import at top of this file
-		let webglLint = this.webglLint = this.gl.getExtension('GMAN_debug_helper');
-		if (webglLint) {
-			webglLint.setConfiguration({
-				maxDrawCalls: 2000,
-				failUnsetSamplerUniforms: true,
-			});
-			this.tagObject = webglLint.tagObject.bind(webglLint)
-		}
-		else {
-			// ext doesn't work i guess
-			this.tagObject = () => {};
-		}
+		// webgl-lint extension, needs import at top of this file.  Otherwise,
+		// getExtension returns null and tagObject() is an empty function, so no probs.
+//		let webglLint = this.webglLint = this.gl.getExtension('GMAN_debug_helper');
+//		if (webglLint) {
+//			webglLint.setConfiguration({
+//				maxDrawCalls: 2000,
+//				failUnsetSamplerUniforms: true,
+//			});
+//			this.tagObject = webglLint.tagObject.bind(webglLint)
+//		}
+//		else {
+//			// ext doesn't work i guess
+		this.tagObject = () => {};
+//		}
+
 	}
 
 	// try to set up GL1, return falsy if it can't.  Also shim the vao methods
@@ -78,6 +86,7 @@ class glAux {
 		gl.createVertexArray = vaoExt.createVertexArrayOES.bind(vaoExt);
 		gl.deleteVertexArray = vaoExt.deleteVertexArrayOES.bind(vaoExt);
 		gl.bindVertexArray = vaoExt.bindVertexArrayOES.bind(vaoExt);
+		this.WebGLVersion = 1;
 		if (traceVersion)
 			console.log(`enabled original WebGL 1`);
 		return gl;
@@ -89,12 +98,22 @@ class glAux {
 		if (!gl)
 			return null;
 		this.gl = gl;
+		this.WebGLVersion = 2;
 		if (traceVersion)
 			console.log(`enabled WebGL 2`);
 		return gl;
 	}
 
+	// not sure if any of these are useful - seems to only address context loss; when does this happen?
+	listenerFunc =
+	ev => console.warn(`WebGL event '${ev.type}':`, ev);
 
+	attachEventHandlers() {
+		const c = this.canvas;
+		c.addEventListener('webglcontextlost', this.listenerFunc);
+		c.addEventListener('webglcontextrestored', this.listenerFunc);
+		c.addEventListener('webglcontextcreationerror', this.listenerFunc);
+	}
 };
 
 
