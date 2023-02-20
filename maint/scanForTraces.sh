e6ab81bb12232fbb9f7c0c355f904db5ab09dbbd
Allan Bonadio <bojnac@tactileint.org>
2023-02-19 17:52:47 -0800

    now prints out GL line numbers for visual approval (if any) also all greps now ignore bin files

--- maint/scanForTraces.sh
+++ maint/scanForTraces.sh
@@ -3,15 +3,20 @@
 echo "Scan sources for traces i left on... returns failure if there's some"
 echo "🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦  C++"
 cd $SQUISH_ROOT/quantumEngine
-grep   -ERn 'bool trace.* = true;' * && \
+grep   -ERnI 'bool trace.* = true;' * && \
 	echo "Try global regex search     bool trace.* = true;  "
 
 echo
 echo "🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦  JavaScript"
 
 cd $SQUISH_ROOT/src
-grep  -ERn '^(let|const) trace.* = true;' * && \
+grep  -ERnI '^(let|const) trace.* = true;' * && \
 	echo "Try global regex search     ^(let|const) trace.* = true;  "
+
+echo "🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦  GL line numbers"
+
+cd $SQUISH_ROOT/src/gl
+grep  -ERnIA2 '^#line \d+' *
 
 echo "🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 🟦 done scaning sources"
 
