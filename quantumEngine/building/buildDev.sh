a46c3c2c0ff2f6ff9e54d1ce3b85ad7c229eb96a
Allan Bonadio <bojnac@tactileint.org>
2023-02-22 10:19:23 -0800

    a few leftover details sorry

--- quantumEngine/building/buildDev.sh
+++ quantumEngine/building/buildDev.sh
@@ -26,8 +26,8 @@ fi
 export EMSDK_QUIET=1
 . $qEMSCRIPTEN/emsdk/emsdk_env.sh
 
-+# most c++ files, except main.cpp, testing files, worker files.
-+# omit those, so testing can also use this and compile & run itself (see testing/cppu*).
+# most c++ files, except main.cpp, testing files, worker files.
+# omit those, so testing can also use this and compile & run itself (see testing/cppu*).
 allCpp=`cat building/allCpp.list`
 
 # keep MAX_LABEL_LEN+1 a multiple of 4 or 8 for alignment, eg 7, 15 or 32
@@ -49,7 +49,7 @@ emcc -o wasm/quantumEngine.js -sLLD_REPORT_UNDEFINED \
 	-DqDEV_VERSION -DMAX_LABEL_LEN=$MAX_LABEL_LEN -DMAX_DIMENSIONS=$MAX_DIMENSIONS \
 	-I$qEMSCRIPTEN/emsdk/upstream/emscripten/cache/sysroot/include \
 	-include emscripten.h -include squish.h \
-	-ffast-math  -lembind --proxy-to-worker \
+	-ffast-math  -lembind \
 	main.cpp $allCpp || exit $?
 # changed -g to -g4 to -gsource-map --source-map-base / ; debugger can see into c++
 
