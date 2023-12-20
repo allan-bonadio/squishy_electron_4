db86af59835f8ab240160cb82e2f92ecdf5aa654
Allan Bonadio <git@orgmgr.tactileint.org>
2023-12-20 00:10:08 -0800

    took out the quantumEngine/js directory; no longer beating my head against the wall to get emscripten workers going.

--- quantumEngine/js/postjs.js
+++
@@ -1,7 +0,0 @@
-/*
-** post js - code to append to the JS that emcc generates
-** Copyright (C) 2023-2023 Tactile Interactive, all rights reserved
-*/
-
-// this does NOT go thru babel/webpack; it's raw-metal javascript
-console.log(`👩‍🌾 Hey!  postjs.js has been included!`)
--- quantumEngine/js/pre-thread.js
+++
@@ -1,7 +0,0 @@
-/*
-** pre js - code to prepend to the JS that emcc generates
-** Copyright (C) 2023-2023 Tactile Interactive, all rights reserved
-*/
-
-// this does NOT go thru babel/webpack; it's raw-metal javascript
-console.log(`💉 pre-thread.js has been included!`)
