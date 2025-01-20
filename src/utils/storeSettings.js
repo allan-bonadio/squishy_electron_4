2a91277d1614fcba71eb993db1f33f31c01e01f2
Allan Bonadio <git@orgmgr.tactileint.org>
2025-01-19 18:58:24 -0800

    changed default dtStretch from 1 to .01

--- src/utils/storeSettings.js
+++ src/utils/storeSettings.js
@@ -229,7 +229,7 @@ export function createStoreSettings() {
 	// set in integration tab
 	makeParam('frameSettings', 'shouldBeIntegrating', true,  [false, true]);
 	makeParam('frameSettings', 'chosenFP', 50, {min: 16, max: 60_001});
-	makeParam('frameSettings', 'dtStretch', 1, {min: .001, max: 1, });
+	makeParam('frameSettings', 'dtStretch', .01, {min: .0001, max: 1, });
 	//makeParam('frameSettings', 'stepsPerFrame', 10, {min: 2, max: 50});
 	//makeParam('frameSettings', 'lowPassFilter', 50, {min: 0, max: 75});
 
