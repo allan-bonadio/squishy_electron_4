fab9770cba88162613ab2f3642fd5fa2e5e4dcd1
Allan Bonadio <git@orgmgr.tactileint.org>
2025-11-19 11:46:04 -0800

    trivial/cleanup/comments/traces/linting/etc

--- src/sPanel/DocMenu.js
+++ src/sPanel/DocMenu.js
@@ -3,14 +3,18 @@
 ** Copyright (C) 2025-2025 Tactile Interactive, all rights reserved
 */
 
+
+
+
 import React from 'react';
 import PropTypes, {checkPropTypes} from 'prop-types';
 
-import './DocReader.js';
-import './DocReader.js';
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+
+import DocReader from './DocReader.js';
 import './DocMenu.scss';
 
-// This is a menu into /public/doc
+// This is a menu into /public/doc for the user to find documentation from the /DocGen subdirectory.
 
 //let traceReader = false;
 
@@ -31,12 +35,12 @@ function DocMenu(props) {
 			return;
 
 		DocReader.openWithUri(`/${topic}/${topic}.html`);
-		target.value = 'doc';
+		target.value = 'docs';
 	}
 
 	return (
 		<select id='DocMenu' onChange={handleSelect}>
-			<option value='doc' >doc</option>
+			<option value='docs' >docs</option>
 			<option value='intro'>Intro</option>
 			<option value='gettingStarted'>Getting Started</option>
 			<option value='naturalWaves'>Natural Waves</option>
@@ -44,6 +48,41 @@ function DocMenu(props) {
 		</select>
 	);
 }
+
+// 	const checked=true
+// 	const checkedChange=()=>{}
+//
+//
+// 	// 'asChild' is required otherwise you get nested buttons
+// 	return (
+// 		<DropdownMenu.Root>
+// 			<DropdownMenu.Trigger className='docsTrigger'>
+// 				docs
+// 			</DropdownMenu.Trigger>
+//
+// 			<DropdownMenu.Content className="docsContent" sideOffset={5}>
+// 				<DropdownMenu.Item className="gettingStartedItem">
+// 					Getting Started
+// 				</DropdownMenu.Item>
+// 				<DropdownMenu.Item className="naturalWavesItem">
+// 					Natural Waves
+// 				</DropdownMenu.Item>
+// 				<DropdownMenu.Item className="digitalWavesItem" disabled>
+// 					Digital Waves
+// 				</DropdownMenu.Item>
+// 			</DropdownMenu.Content>
+// 		</DropdownMenu.Root>
+// 	);
+
+			// <button className="IconButton" aria-label="Customise options">
+			// 	docs
+			// </button>
+
+
+
+
+
+
 
 
 export default DocMenu;
