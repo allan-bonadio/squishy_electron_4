c65d2b185ca910857403a42a8177ebd531520dc8
Allan Bonadio <git@orgmgr.tactileint.org>
2025-12-06 00:05:08 -0800

    waveParams stored ranges: pulse width now maxes out at 20 still min 1 wave frequency now goes -5...-5 rather than -100...100 on

--- src/utils/storeSettings.js
+++ src/utils/storeSettings.js
@@ -197,8 +197,8 @@ export function createStoreSettings() {
 	// truth.
 
 	makeParam('waveParams', 'waveBreed', 'gaussian', ['circular', 'standing', 'gaussian', 'chord']);
-	makeParam('waveParams', 'waveFrequency', 6, {min: -100, max: 100, step: 0.5});
-	makeParam('waveParams', 'pulseWidth', 10, {min: 1, max: 100});
+	makeParam('waveParams', 'waveFrequency', 6, {min: -5, max: 5, step: 0.5});
+	makeParam('waveParams', 'pulseWidth', 10, {min: 1, max: 20});
 	makeParam('waveParams', 'pulseCenter', 20, {min: 0, max: 100});
 
 	/* ************************************ voltage */
