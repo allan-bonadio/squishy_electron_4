cd3c4290345fb740458a90b16c6a7f59d75699f2
Allan Bonadio <git@orgmgr.tactileint.org>
2026-03-25 16:39:58 -0700

    trivial/cleanup/comments/traces/linting/etc

--- src/gl/drawingVariable.js
+++ src/gl/drawingVariable.js
@@ -33,7 +33,7 @@ export class drawingVariable {
 
 		let vv = drawing.drawVariables;
 		if (vv.includes(this)) {
-			console.error(`𒐿 duplicate variable ${varName}!!`);
+			console.error(`🍯 duplicate variable ${varName}!!`);
 			return null;
 		}
 		vv.push(this);
@@ -42,7 +42,7 @@ export class drawingVariable {
 		// or maybe this is already done and so this is superflous?
 		drawing.setDrawing();
 
-		if (traceGLCalls) console.log(`𒐿 created drawingVariable '${varName}', drawing:`, drawing);
+		if (traceGLCalls) console.log(`🍯 created drawingVariable '${varName}', drawing:`, drawing);
 	}
 }
 
@@ -72,7 +72,7 @@ export class drawingUniform extends drawingVariable {
 		this.uniformLoc = this.gl.getUniformLocation(this.drawing.program, varName);
 		if (!this.uniformLoc)
 			throw new Error(`Cannot find uniform loc for uniform variable ${varName}`);
-		if (traceUniforms) console.log(`𒐿 created drawingUniform '${varName}'`);
+		if (traceUniforms) console.log(`🍯 created drawingUniform '${varName}'`);
 
 		this.reloadVariable();
 	}
@@ -84,7 +84,7 @@ export class drawingUniform extends drawingVariable {
 		// is passed the previous value
 		const {value, type} =  this.reloadFunc();
 		if (traceUniforms)
-			console.log(`𒐿 re-loading uniform ${this.varName} with value=${value} type ${type}`);
+			console.log(`🍯🍯 re-loading uniform ${this.varName} with value=${value} type ${type}`);
 
 		// you can't pass null or undefined to webgl, but remember this
 		this.value = value;
@@ -93,7 +93,7 @@ export class drawingUniform extends drawingVariable {
 			// better luck next time.
 			let msg = `uniform variable ${this.varName} has `
 						+`a problematic value(${value}) or no type(${type})`;
-			console.warn(`𒐿 ${msg}`);
+			console.warn(`🍯 ${msg}`);
 			this.drawing.skipDrawingCuzErr = msg;
 			return;
 		}
@@ -113,7 +113,7 @@ export class drawingUniform extends drawingVariable {
 		gl[method].apply(gl, args);
 
 		if (traceUniforms) {
-			console.log(`𒐿 drawingUniform reloaded U variable '${this.varName}' in `);
+			console.log(`🍯 drawingUniform reloaded U variable '${this.varName}' in `);
 			console.log(`            ${this.drawing.avatarLabel} uniform gl.${method}`
 				+`  (${args[0].constructor.name}, ${args[1]}, ${args[2]} ) `);
 		}
@@ -144,7 +144,7 @@ export class drawingAttribute extends drawingVariable {
 		gl.enableVertexAttribArray(this.attrLocation);
 
 		if (this.attrLocation < 0) {
-			throw new Error(`𒐿 drawingAttribute:attr loc for '${varName}' is bad: `+
+			throw new Error(`🍯 drawingAttribute:attr loc for '${varName}' is bad: `+
 				this.attrLocation);
 		}
 
@@ -165,7 +165,7 @@ export class drawingAttribute extends drawingVariable {
 		gl.vertexAttribPointer(this.attrLocation, tupleWidth, gl.FLOAT,
 				false, tupleWidth * 4, 0);  // normalize, stride, offset
 		if (traceGLCalls)
-			console.log(`𒐿  drawingAttribute '${this.varName}' connected to its `
+			console.log(`🍯  drawingAttribute '${this.varName}' connected to its `
 				+` glBuffer to tupleWidth=${tupleWidth}`);
 
 		// we haven't touched the actual JS data yet, this does it.  Although a lot of
@@ -193,7 +193,7 @@ export class drawingAttribute extends drawingVariable {
 		gl.bufferData(gl.ARRAY_BUFFER, floatArray, gl.DYNAMIC_DRAW);
 
 		if (traceAttrs || traceAttributes)
-			console.log(`𒐿 reload drawingAttribute '${this.varName}' in ${this.drawing.sceneName}`
+			console.log(`🍯 reload drawingAttribute '${this.varName}' in ${this.drawing.sceneName}`
 				+ ` reloaded, ${this.nTuples}  tuples of ${this.tupleWidth} floats each:`);
 
 		if (traceAttributes) {
