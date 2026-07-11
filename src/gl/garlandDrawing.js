a83570492d43d55b280bcd6cb72e1355a7f8e4a2
Allan Bonadio <git@orgmgr.tactileint.org>
2026-07-10 20:08:22 -0700

    trivial/cleanup/comments/traces/linting/etc

--- src/gl/garlandDrawing.js
+++ src/gl/garlandDrawing.js
@@ -16,7 +16,7 @@ let traceAvatarAfterDrawing = false;
 let traceSimulateGL = false;
 let traceDrawing = false;
 let traceReloadRow = false;
-let traceMatrix = true;
+let traceMatrix = false;
 let traceUColor = false;
 
 // diagnostic purposes; draws more per vertex
