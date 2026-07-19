3f25b9d74969720fa75e2cc50591a3d04710a9cb
Allan Bonadio <git@orgmgr.tactileint.org>
2026-07-18 21:26:11 -0700

    continuing vain attempts to get pivot UI to work

--- src/wave/PivotLayer.js
+++ src/wave/PivotLayer.js
@@ -7,10 +7,11 @@ import React, {useContext, useRef} from 'react';
 import PropTypes from 'prop-types';
 
 // one mouse unit equals how many screen pixel units?
-// or, um, the angle in degrees it should equate to
+// or, um, the angle in degrees it should equate to.  Adjust to taste.
 let X_PIX_RATIO = .005;
 let Y_PIX_RATIO = .005;
 
+// round off passed  angle to factor of 5°
 const roundAngle = (theta) => Math.max(-90, Math.min(90, Math.round(theta/5) * 5));
 
 
@@ -19,8 +20,9 @@ const propTypes = {
 	// fields (not just the ones listed here): {xyz}{Ang,Pos}
 	orient: PropTypes.shape({
 		xAng: PropTypes.number.isRequired,
-		yPos: PropTypes.number.isRequired,
-		hfoView: PropTypes.number,
+		yAng: PropTypes.number.isRequired,
+		zAng: PropTypes.number.isRequired,
+		//hfoView: PropTypes.number,
 		// plus others i'm too lazy to do
 	}),
 
@@ -33,6 +35,33 @@ export function PivotLayer(props) {
 	const pointerDownRef = useRef();
 	let pointerDown = pointerDownRef.current;
 
+
+	const downHandler = (ev) => {
+		if (0 == ev.buttons)
+			return;
+
+
+		if (!pointerDown) {
+			// must be starting - mouse down, or mouse enter
+			// remember where mouse down was and what angles that meant
+			pointerDown = pointerDownRef.current = {x: ev.clientX, y: ev.clientY,
+				xAng: props.orient.xAng, yAng: props.orient.yAng};
+		}
+
+		// ok now, how far has it moved and in what direction? measure
+		// from mousedown point.  Note:xAng and yAng need to be
+		// reversed: xAng is rotation around y axis, etc.
+		let delMouseY = ev.clientX - pointerDown.x;
+		let delMouseX = ev.clientY - pointerDown.y;
+
+		// so that means new angles are:
+		props.setAngSetting('xAng',
+			roundAngle(props.orient.xAng + delMouseX * X_PIX_RATIO));
+			//Math.round(props.orient.xAng + delMouseX * X_PIX_RATIO));
+		props.setAngSetting('yAng',
+			roundAngle(props.orient.yAng + delMouseY * Y_PIX_RATIO));
+			//Math.round(props.orient.yAng + delMouseY * Y_PIX_RATIO));
+	}
 
 	const moveHandler = (ev) => {
 		if (0 == ev.buttons) {
@@ -62,6 +91,7 @@ export function PivotLayer(props) {
 			roundAngle(props.orient.yAng + delMouseY * Y_PIX_RATIO));
 			//Math.round(props.orient.yAng + delMouseY * Y_PIX_RATIO));
 	}
+
 
 	return <section className='PivotLayer'
 		onPointerMove={moveHandler} />
